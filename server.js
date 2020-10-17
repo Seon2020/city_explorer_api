384745e1d3933b937ccc78545a2a0d0adf16a221
Sean <seanh@zillowgroup.com>
2020-10-17 12:40:21 -0700

    finished feature 4

--- server.js
+++ server.js
@@ -20,21 +20,31 @@ app.use(cors());
 
 //Location Route 
 app.get('/location', (request, response) => {
+  try {
   let city = request.query.city;
   //Get data from source
   let data = require('./data/location.json')[0];
   let location = new Location(data, city);
   response.send(location);
+  } 
+    catch (error) {
+      handleError();
+    }
 });
 
 // Weather Route
 app.get('/weather', (request, response) => {
+  try {
   let forecast = require('./data/weather.json');
   let weatherArr = [];
   forecast.data.forEach(value => {
     weatherArr.push(new Weather(value));
   });
   response.send(weatherArr);
+}
+  catch (error) {
+    handleError();
+  }
 });
 
 
@@ -52,6 +62,11 @@ function Weather(obj) {
   this.forecast = obj.weather.description;
   this.time = new Date(obj.valid_date).toDateString();
 }
+
+//Error function
+function handleError(){
+return response.status(500).send("Sorry, something went wrong");
+};
 
 // Start server
 app.listen(PORT, () => {
